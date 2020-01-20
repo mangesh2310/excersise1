b9a15ebeb95a14beab96a39200ef688fb5dd3f0d
ASUS <mangesh.swami94@gmail.com>
2020-01-20 22:44:10 +0530

    my changes

---
+++ src/test/java/com/ek/trainings/jenkins/JenkinsCalTest.java
@@ -0,0 +1,23 @@
+package com.ek.trainings.jenkins;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JenkinsCalTest {
+	
+	@Test
+	public void addTest()
+	{
+		JenkinsCalculator myCalc = new JenkinsCalculator();
+		assertEquals(10,myCalc.addNumbers(5,5));
+	}
+	
+	@Test
+	public void subtractTest()
+	{
+		JenkinsCalculator myCalc = new JenkinsCalculator();
+		assertEquals(5,myCalc.subtractNumbers(10,5));
+	}
+
+}
